7406e249e44fcd2b19e1e03b7265e76dcbb4e45e
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-30 22:11:47 +0530

    Display high volume stocks if available

--- app.py
+++ app.py
@@ -155,5 +155,12 @@ else:
     st.info("No Daily Breakout Today")
 with col4:
     st.subheader("🔥 Last 5 Days High Volume")
-    st.info("Coming Soon")
+    if high_volume:
+    st.dataframe(
+        pd.DataFrame(high_volume),
+        use_container_width=True,
+        hide_index=True
+    )
+else:
+    st.info("No High Volume Stocks Today")
 
