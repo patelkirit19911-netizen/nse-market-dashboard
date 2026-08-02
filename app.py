2eb6001c934c513b15509df1c674a7d46718af96
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-08-02 17:03:33 +0530

    Refactor stock signal logic in app.py
    Refactor the logic for appending stock signals to orb_scanner based on current price.

--- app.py
+++ app.py
@@ -252,21 +252,21 @@ for stock in sector_df["SYMBOL"]:
                 change_pct = round(((current_price - previous_close) / previous_close) * 100,2)
 
                 sector = sector_df.loc[sector_df["SYMBOL"] == stock,"SECTOR"].values[0]
-                if current_price > orb_high:
-                    orb_scanner.append({
-                    "Stock": stock,
-                    "Sector": sector,
-                    "Current Price": round(current_price, 2),
-                    "%": f"{change_pct:+.2f}%",
-                    "Signal": "🟢 BUY"
-                    })
-                 elif current_price < orb_low:
-                       orb_scanner.append({
-                       "Stock": stock,
-                       "Sector": sector,
-                       "Current Price": round(current_price, 2),
-                       "%": f"{change_pct:+.2f}%",
-                       "Signal": "🔴 SELL"})
+                    if current_price > orb_high:
+                        orb_scanner.append({
+                        "Stock": stock,
+                        "Sector": sector,
+                        "Current Price": round(current_price, 2),
+                        "%": f"{change_pct:+.2f}%",
+                        "Signal": "🟢 BUY"})
+                        
+                     elif current_price < orb_low:
+                           orb_scanner.append({
+                           "Stock": stock,
+                           "Sector": sector,
+                           "Current Price": round(current_price, 2),
+                           "%": f"{change_pct:+.2f}%",
+                           "Signal": "🔴 SELL"})
 
             if yesterday_high <= previous_week_high and today_high > previous_week_high:
 
