4f8291bef2edf4d8bb003bbc1419f367af3d944b
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-08-03 20:21:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,43 +60,7 @@ for _, row in chart_df.iterrows():
     sector = row["Sector"]
     change = row["Change"]
 
-    with st.expander(f"{sector} ({change:.2f}%)"):
-
-        sector_stocks = sector_df[
-            sector_df["SECTOR"] == sector
-        ]["SYMBOL"].tolist()
-
-        gainers = []
-        losers = []
-
-        for stock in sector_stocks:
-            try:
-                data = yf.Ticker(stock + ".NS").history(period="2d")
-
-                if len(data) >= 2:
-                    chg = (
-                        (data["Close"].iloc[-1] - data["Close"].iloc[-2])
-                        / data["Close"].iloc[-2]
-                    ) * 100
-
-                    if chg >= 0:
-                        gainers.append((stock, chg))
-                    else:
-                        losers.append((stock, chg))
-            except:
-                pass
-
-        c1, c2 = st.columns(2)
-
-        with c1:
-            st.success("🟢 Gainers")
-            for s, c in sorted(gainers, key=lambda x: x[1], reverse=True):
-                st.write(f"{s}  {c:.2f}%")
-
-        with c2:
-            st.error("🔴 Losers")
-            for s, c in sorted(losers, key=lambda x: x[1]):
-                st.write(f"{s}  {c:.2f}%")
+    
 
 
 col3, col4 = st.columns([3,2])
